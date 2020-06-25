ed2766a30ff21766d3995adbf8546e37dbe79313
MarCanovas <canovasgarciamar@gmail.com>
2020-06-25 21:28:57 +0200

    2 player functionality implemented

--- your-project/main.py
+++ your-project/main.py
@@ -1,20 +1,38 @@
 import numpy as np
 
-
+#-----------------------------------
 # VARIABLES
+#-----------------------------------
 
 board = np.array([[" "," "," "],[" "," "," "],[" "," "," "]])
 
 player = ""
 
-switcher_player = {'O':'player', 'X':'machine'}
+switcher_player = {'O':'Player1', 'X':'Player2'}
+
+move_translate = {
+	'ul' : (0,0),	'lu' : (0,0),
+	'um' : (0,1),	'mu' : (0,1),
+	'ur' : (0,2),	'ru' : (0,2),
+
+	'cl' : (1,0),	'lc' : (1,0),
+	'cm' : (1,1),	'mc' : (1,1),
+	'cr' : (1,2),	'rc' : (1,2),
+
+	'bl' : (2,0),	'lb' : (2,0),
+	'bm' : (2,1),	'mb' : (2,1),
+	'br' : (2,2),	'rb' : (2,2)
+}
 
 winner = ""
 
+#-----------------------------------
 # FUNCTIONS
+#-----------------------------------
+
 
 # Behaviour of the game at its start
-def game_info(player):
+def game_info():
 
 	print("      -----------------------------")
 	print("      |  Welcome to TIC TAC TOE!  |")
@@ -35,8 +53,10 @@ def game_info(player):
 
 	print()
 
+	player = ""
+
 	while not (player == "O") and not (player == "X"):
-		print("Who is starting? Player [O] or Machine [X]?")
+		print("Who is starting? Player1 [O] or Player2 [X]?")
 		player = input();
 
 	return player
@@ -53,18 +73,21 @@ def draw_board():
 	print("         -----------------------")
 
 
+#-----------------------------------
+
+
 def make_move(player, move):
-	if (move == 'ul') or (move == 'lu') : board[0,0] = player
-	elif (move == 'um') or (move == 'mu') : board[0,1] = player
-	elif (move == 'ur') or (move == 'ru') : board[0,2] = player
+	m = move_translate[move]
+	board[m[0],m[1]] = player
 
-	elif (move == 'cl') or (move == 'lc') : board[1,0] = player
-	elif (move == 'cm') or (move == 'mc') : board[1,1] = player
-	elif (move == 'cr') or (move == 'rc') : board[1,2] = player
+	if player == "X": return "O"
+	elif player == "O": return "X"
 
-	elif (move == 'bl') or (move == 'lb') : board[2,0] = player
-	elif (move == 'bm') or (move == 'mb') : board[2,1] = player
-	elif (move == 'br') or (move == 'rb') : board[2,2] = player
+
+def check_not_valid_move(move):
+	m = move_translate[move]
+	if board[m[0],m[1]] != " " : return 1
+	else: return 0
 
 
 
@@ -72,25 +95,61 @@ def player_move(player):
 	print("Please enter your move:")
 	move = str(input())
 
+	while check_not_valid_move(move):
+		print("Occupied cell, choose one free:")
+		move = str(input())
+
 	make_move(player, move)
 
 	draw_board()
 
-	return "X"
-
-def machine_move(player):
-	print("Machine's move:")
-
-	return "O"
+	if player == "O" : return "X"
+	elif player == "X" : return "O"
 
 
+#-----------------------------------
+
+
+def check_winner():
+	winner = ""
+
+	#ROWS
+	if (board[0,0] == board[0,1]) and (board[0,0] == board[0,2]) and (not board[0,0] == " "):
+		winner = board[0,0]
+	if (board[1,0] == board[1,1]) and (board[1,0] == board[1,2]) and (not board[1,0] == " "):
+		winner = board[1,0]
+	if (board[2,0] == board[2,1]) and (board[2,0] == board[2,2]) and (not board[2,0] == " "):
+		winner = board[2,0]
+	
+	#COLUMNS
+	if (board[0,0] == board[1,0]) and (board[0,0] == board[2,0]) and (not board[0,0] == " "):
+		winner = board[0,0]
+	if (board[0,1] == board[1,1]) and (board[0,1] == board[2,1]) and (not board[0,1] == " "):
+		winner = board[0,1]
+	if (board[0,2] == board[1,2]) and (board[0,2] == board[2,2]) and (not board[0,2] == " "):
+		winner = board[0,2]
+
+	#DIAGONALS
+	if (board[0,0] == board[1,1]) and (board[0,0] == board[2,2]) and (not board[0,0] == " "):
+		winner = board[0,0]
+	if (board[0,2] == board[1,1]) and (board[0,2] == board[2,0]) and (not board[0,2] == " "):
+		winner = board[0,2]
+
+	return winner
+
+
+
+#-----------------------------------
 # PLAYING LOOP
-player = game_info(player)
+#-----------------------------------
+
+player = game_info()
 
 print ("Ok, is "+ str(switcher_player.get(player)) + "'s turn:" )
 
 while (winner == ""):
-	if player == 'O' : player = player_move(player)
-	elif player == 'X' : player = machine_move(player)
-
+	player = player_move(player)
+	winner = check_winner()
+	
+print ("The winner is "+ winner +". Congratulations!")
 
